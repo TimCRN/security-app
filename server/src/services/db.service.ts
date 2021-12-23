356522af50b014730c5570e336a26041b4bed13a
TimCRN <tim.crounse@gmail.com>
2021-12-23 15:27:47 +0100

    Added dbname entry

--- server/src/services/db.service.ts
+++ server/src/services/db.service.ts
@@ -5,13 +5,14 @@ if (process.env.ENV !== 'prod') {
   require('dotenv').config();
 }
 
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_USER = process.env.DB_USER;
-const DB_HOSTNAME = process.env.DB_HOSTNAME;
+const password = process.env.DB_PASSWORD;
+const user = process.env.DB_USER;
+const hostname = process.env.DB_HOSTNAME;
+const dbname = process.env.DB_DBNAME;
 
-export const checkDBConnection = async () => {
+export const connectDB = async () => {
   try {
-    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}`);
+    await mongoose.connect(`mongodb+srv://${user}:${password}@${hostname}/${dbname}`);
     console.log('DB connection success!');
   } catch(error) {
     console.error(error);
