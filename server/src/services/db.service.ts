e2557966155051eb9fd674fa69b4dddc442f00ad
TimCRN <tim.crounse@gmail.com>
2021-12-23 12:40:07 +0100

    Added mongoose connection

--- server/src/services/db.service.ts
+++ server/src/services/db.service.ts
@@ -1,6 +1,19 @@
+import mongoose from 'mongoose';
+
 // Load environment variables in non-production environment
 if (process.env.ENV !== 'prod') {
   require('dotenv').config();
 }
 
-export const checkDBConnection = async () => {};
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_USER = process.env.DB_USER;
+const DB_HOSTNAME = process.env.DB_HOSTNAME;
+
+export const checkDBConnection = async () => {
+  try {
+    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}`);
+    console.log('DB connection success!');
+  } catch(error) {
+    console.error(error);
+  }
+};
