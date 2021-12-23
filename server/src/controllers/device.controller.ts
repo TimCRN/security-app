07a971e17db13cd46008c409fa1d6e4d65760d71
TimCRN <tim.crounse@gmail.com>
2021-12-23 15:46:07 +0100

    Updated device controller

--- server/src/controllers/device.controller.ts
+++ server/src/controllers/device.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 import { Device, DeviceInput } from '../models/devices.model';
+import { devicesRouter } from '../routes/devices.route';
 
-export class DeviceController {
-    async createDevice(req: Request, res: Response) {
+export class DeviceController 
+{
+    
+    async createDevice(req: Request, res: Response) 
+    {
         const { name } = req.body;
 
         if(!name)
@@ -17,10 +21,46 @@ export class DeviceController {
         }
 
         const deviceCreated = await Device.create(input)
-        return res.status(201).json({data: deviceCreated});
+        return res.status(201).json(deviceCreated);
     }
-    async getAllDevices(req: Request, res: Response) {
+
+
+    async getAllDevices(req: Request, res: Response) 
+    {
         const devices = await Device.find();
         return res.status(201).json(devices);
+    }
+
+
+    async getDevice(req: Request, res: Response) 
+    {
+        const { id } = req.params;
+
+        const device = await Device.findById(id);
+
+        if(!device)
+        {
+            return res.status(404).json(`Device with ID '${id}' does not exist.`);
+        }
+        return res.status(201).json(device);
+    }
+
+    async updateDevice(req: Request, res: Response)
+    {
+        return res.status(201).json();
+    }
+
+    async deleteDevice(req: Request, res: Response) 
+    {
+        const { id } = req.params;
+
+        const device = await Device.findById(id);
+
+        if(!device)
+        {
+            return res.status(404).json(`Device with ID '${id}' can not be deleted, because it does not exist.`);
+        }
+        await Device.findByIdAndDelete(id);
+        return res.status(201).json(`Device '${id}' has been succesfully deleted!`);
     }
 }
