34b5197251ae07e6037f891fe4550972147bebb5
TimCRN <tim.crounse@gmail.com>
2021-12-23 20:46:16 +0100

    Added HttpClientModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DevicesComponent } from './components/devices/devices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevicesComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule, 
     AppRoutingModule
   ],
   providers: [],
