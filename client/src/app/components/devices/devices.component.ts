b8fb6a05859dd5aa27fc87e6be38ccfef59e9b82
TimCRN <tim.crounse@gmail.com>
2021-12-24 19:31:11 +0100

    Added device creation and removal logic

--- client/src/app/components/devices/devices.component.ts
+++ client/src/app/components/devices/devices.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DeviceService } from '../../services/device.service';
 import { Device } from '../../models/device.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-devices',
@@ -10,13 +12,51 @@ import { Device } from '../../models/device.model';
 })
 export class DevicesComponent implements OnInit {
   devices!: Device[];
+  newDeviceForm!: FormGroup;
 
-  constructor(private deviceService: DeviceService) {}
+  constructor(
+    private deviceService: DeviceService,
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.deviceService.getDevices()
-      .subscribe(devices => 
-      this.devices = devices);
+    .subscribe(devices => 
+    this.devices = devices);
   }
 
+  onSubmit()
+  {
+    
+  }
+
+  addDevice()
+  {
+    this.deviceService.addDevice().subscribe()
+    this.toastr.success(`Device has been added`)
+  }
+
+  getDevice(id: any)
+  {
+
+  }
+
+  editDevice(id: any){
+
+  }
+
+  removeDevice(id: any) {
+    this.deviceService.removeDevice(id)
+      .subscribe(data =>{
+        for(var i = 0; i < this.devices.length; i++)
+        {
+          if(this.devices[i]._id == id)
+          {
+            this.devices.splice(i,1);
+          }
+        }
+      })
+    this.toastr.success(`Device '${id}' has been deleted`)
+  }
 }
