ee569df3510ecf259c351ce5c3c953ffe86723cf
Niels Kersic <hi@niels.codes>
2022-01-08 16:41:29 +0100

    Add route guard
    Protect routes from unauthorized users with AngularFire auth guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { LoginComponent } from './routes/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login'])
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent,...canActivate(redirectUnauthorizedToLogin) },
   { path: 'login', component: LoginComponent }
 ];
 
