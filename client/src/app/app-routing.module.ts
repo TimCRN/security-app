1981b309d52aa1114cdfc48db3752491775871e4
Niels Kersic <hi@niels.codes>
2022-01-10 12:06:44 +0100

    Add redirect for logged in users
    If a user is logged in, they are redirected to home when trying to visit /login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { LoginComponent } from './routes/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
-import { canActivate, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login'])
-
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
 const routes: Routes = [
   { path: '', component: HomeComponent,...canActivate(redirectUnauthorizedToLogin) },
-  { path: 'login', component: LoginComponent }
+  // TODO: Redirect to home when already logged in
+  { path: 'login', component: LoginComponent, ...canActivate(redirectLoggedInToHome) }
 ];
 
 @NgModule({
