da32e3382494abc9a8dea9a522c0d42b147f71e6
Niels Kersic <kersic.niels@gmail.com>
2022-01-17 13:07:18 +0100

    Revert "Mongo db"

--- server/src/services/db.service.ts
+++ server/src/services/db.service.ts
@@ -1,20 +1,21 @@
-import mongoose from 'mongoose';
+import {Sequelize} from 'sequelize';
 
 // Load environment variables in non-production environment
 if (process.env.ENV !== 'prod') {
   require('dotenv').config();
 }
 
-const password = process.env.DB_PASSWORD;
-const user = process.env.DB_USER;
-const hostname = process.env.DB_HOSTNAME;
-const dbname = process.env.DB_DBNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const sequelize = new Sequelize(
+  // eslint-disable-next-line prettier/prettier
+  `postgresql://test:${DB_PASSWORD!}@free-tier5.gcp-europe-west1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&sslrootcert=./certs/root.crt&options=--cluster%3Dwhite-zebra-2524`
+);
 
-export const connectDB = async () => {
+export const checkDBConnection = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://${user}:${password}@${hostname}/${dbname}`);
-    console.log('DB connection success!');
-  } catch(error) {
+    await sequelize.authenticate();
+    console.log('SQL connection established');
+  } catch (error) {
     console.error(error);
   }
 };
