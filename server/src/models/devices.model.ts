cf3a6a232330ba490faf88624355b0bdc7a5e091
TimCRN <tim.crounse@gmail.com>
2022-01-19 17:19:32 +0100

    Updated Device Schema

--- server/src/models/devices.model.ts
+++ server/src/models/devices.model.ts
@@ -1,18 +1,50 @@
 import * as mongoose from "mongoose";
+import { StringLiteralLike } from "typescript";
 
 export type DeviceDocument = mongoose.Document & {
+    _id: string;
     name: string;
+    model: string;
+    category: string;
+    online: boolean;
+    status: deviceStatus[];
 }
 
 export type DeviceInput = {
+    _id: DeviceDocument['_id'];
     name: DeviceDocument['name'];
+    model: DeviceDocument['model'];
+    category: DeviceDocument['category'];
+    online: DeviceDocument['online'];
+    status: DeviceDocument['status'];
+}
+
+export type deviceStatus = {
+    code: string
+    value: string
 }
 
 const deviceSchema = new mongoose.Schema(
     {
+        _id: {
+            type: String,
+            required: true
+        },
         name: {
             type: String,
             required: true
+        },
+        model: {
+            type: String
+        },
+        category: {
+            type: String
+        },
+        online: {
+            type: Boolean
+        },
+        status: {
+            type: mongoose.Schema.Types.Mixed
         }
     },
     {
@@ -22,3 +54,4 @@ const deviceSchema = new mongoose.Schema(
 );
 
 export const Device = mongoose.model('Device', deviceSchema);
+
