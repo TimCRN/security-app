76a5ec880a5e649a49e8be9a19d13a4b93b89a75
Niels Kersic <hi@niels.codes>
2022-01-27 14:48:46 +0100

    Fix modal close routing

--- client/src/app/components/event-modal/event-modal.component.ts
+++ client/src/app/components/event-modal/event-modal.component.ts
@@ -1,5 +1,6 @@
 import { INotification } from 'src/app/services/api.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-modal',
@@ -10,15 +11,29 @@ export class EventModalComponent implements OnInit {
 
   @Input() event!: INotification | null;
 
+  cameFromPush = false;
+
   constructor(
-  ) { }
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    route.queryParamMap.subscribe(params => {
+      if (params.has('src') && params.get('src') === 'push') {
+        this.cameFromPush = true;
+      }
+    })
+  }
 
   ngOnInit(): void {
+    console.log(this.event);
   }
 
   onCloseModal() {
-    // TODO: implement routing to home if source is push notification
-    window.history.back();
+    if (this.cameFromPush) {
+      this.router.navigate(['/']);
+    } else {
+      window.history.back();
+    }
   }
 
 }
